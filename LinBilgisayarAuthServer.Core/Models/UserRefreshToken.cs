cdc05b41eb4401844d4bf3e89bc2c386ecddb3a6
Emre Pirvan <emreprvn@gmail.com>
2022-06-14 00:09:34 +0300

    Add project files.

---
+++ LinBilgisayarAuthServer.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinBilgisayarAuthServer.Core.Models
+{
+    public class UserRefreshToken
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; } //Refresh Token
+        public DateTime Expiration { get; set; }
+
+    }
+}
